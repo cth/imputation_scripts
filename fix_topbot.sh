ae0f92612691a2e0708d1adf6f7f91f77ac1edd4
Christian Theil Have <c.have@sund.ku.dk>
2014-12-17 09:19:34 +0100

    removed some old scripts

--- fix_topbot.sh
+++
@@ -1,5 +0,0 @@
-
-#plink19 --bfile prepare_plink/hwe10e-4 --update-alleles reverse_allele/rev_alleles.list --make-bed --out prepare_plink/reversed_alleles
-
-sh update_build.sh prepare_plink/hwe10e-4 HumanCoreExome-12v1-0_B-b37.strand prepare_plink/strandupdated 
-
