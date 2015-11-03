23818d5728c7e6aa0b7547ccdda23a4e2477de84
Christian Theil Have <c.have@sund.ku.dk>
2015-11-03 08:38:52 +0100

    cleanup

--- chunked_imputation.rb
+++
@@ -1,6 +0,0 @@
-# Create sge scripts for chunked imputation
-
-require_relative 'df'
-require_relative 'chunking.rb'
-
-1.upto(23) { |i| Chunking::infer_chunks(i).each { |chunk| Chunking::make_script(c*chunk) } }
