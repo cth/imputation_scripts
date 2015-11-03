4c9a96e4df7cd59f493d5ff546504ccc22520fcb
Christian Theil Have c.have@sund.ku.dk <cth@fe1.genomedk.net>
2015-11-03 08:45:03 +0100

    run it

--- 04_chunking.rb
+++ 04_chunking.rb
@@ -7,6 +7,6 @@ puts $cfg.inspect
 $cfg.chromosomes.each do |chr| 
 	Chunking::infer_chunks(chr).each do |chunk| 
 		script=Chunking::make_script(*chunk)
-		#`sbatch -p normal -n1 -c1 #{script}`
+		`sbatch -p normal -n1 -c1 #{script}`
 	end
 end
