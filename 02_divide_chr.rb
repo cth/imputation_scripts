0aeb34488caf2e213f2c78cbf92a9adc0597fbf4
Christian Theil Have <c.have@sund.ku.dk>
2016-04-08 13:47:48 +0200

    flexible plink file names with qc params

--- 02_divide_chr.rb
+++ 02_divide_chr.rb
@@ -7,7 +7,7 @@ $cfg.chromosomes.each do |chr|
 	File.open(script, "w")) do |file|
 		file.puts "#!/bin/bash"
 		file.puts "\#$ -S /bin/bash"
-		file.puts "#{$cfg.plink} --bfile plink/all_hwe10e_maf001_geno005 --chr #{chr} --make-bed --out #{$cfg.unphased_stem(chr)}"
+		file.puts "#{$cfg.plink} --bfile plink/qc-#{hash_param_str(qc_opts)} --chr #{chr} --make-bed --out #{$cfg.unphased_stem(chr)}"
 	end
 	`sbatch -p normal -n1 -c1 sge/chrsplit.#{chr}.sge`
 done
