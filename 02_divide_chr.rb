012078ab0f507c304e1ec5045d3a4c2e2e8e5bee
Christian Theil Have <c.have@sund.ku.dk>
2016-04-08 13:51:02 +0200

    02

--- 02_divide_chr.rb
+++ 02_divide_chr.rb
@@ -1,14 +1,19 @@
-#!/bin/sh
-plink=`../common/software/plink`
+require_relative 'cfg.rb'
+require_relative 'lib/utils.rb'
+
+hsh=({ :maf => $cfg.QC_maf, :mind => $cfg.QC_mind, :geno => $cfg.QC_geno, :hwe => $cfg.QC_hwe })
 
 # Divide by chromosome
 $cfg.chromosomes.each do |chr| 
 	script="sge/chrsplit.#{chr}.sge"
-	File.open(script, "w")) do |file|
-		file.puts "#!/bin/bash"
-		file.puts "\#$ -S /bin/bash"
-		file.puts "#{$cfg.plink} --bfile plink/qc-#{hash_param_str(qc_opts)} --chr #{chr} --make-bed --out #{$cfg.unphased_stem(chr)}"
+	infile="plink/qc-" + hash_param_str(hsh)
+	File.open(script, "w") do |file|
+		file.puts '#!/bin/bash'
+		file.puts '#$ -S /bin/bash'
+		file.puts '#$ -cwd'
+		file.puts "#{$cfg.plink} --bfile #{infile} --chr #{chr} --make-bed --out #{$cfg.unphased_stem(chr)}"
 	end
-	`sbatch -p normal -n1 -c1 sge/chrsplit.#{chr}.sge`
-done
+	`qsub sge/chrsplit.#{chr}.sge`
+	#`sbatch -p normal -n1 -c1 sge/chrsplit.#{chr}.sge`
+end
 
