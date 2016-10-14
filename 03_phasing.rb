8c2d1e0116ba93c4fbb9b504b08a117a6be709c0
cth <christiantheilhave@gmail.com>
2016-10-14 10:51:15 +0200

    effective size option

--- 03_phasing.rb
+++ 03_phasing.rb
@@ -9,7 +9,7 @@ $cfg.chromosomes.each do |chr|
 		file.puts "\#$ -cwd"
 		file.puts "\#$ -pe smp #{$cfg.shapeit_threads}"
 		extra_opts = chr==23 ? "--chrX" : ""
-		file.puts "#{$cfg.shapeit} --input-bed #{$cfg.unphased_bed(chr)} #{$cfg.unphased_bim(chr)} #{$cfg.unphased_fam(chr)} --input-map #{$cfg.map(chr)} -O #{$cfg.phased_haps(chr)} #{$cfg.phased_sample(chr)} --thread #{$cfg.shapeit_threads} -L #{$cfg.phased_log(chr)} #{extra_opts}"
+		file.puts "#{$cfg.shapeit} --input-bed #{$cfg.unphased_bed(chr)} #{$cfg.unphased_bim(chr)} #{$cfg.unphased_fam(chr)} --input-map #{$cfg.map(chr)} -O #{$cfg.phased_haps(chr)} #{$cfg.phased_sample(chr)} --thread #{$cfg.shapeit_threads} --effective-size #{$cfg.shapeit_effective_size} -L #{$cfg.phased_log(chr)} #{extra_opts}"
 	end
 	`qsub #{script}`
 	#`sbatch -p normal -n1 -c16 #{script}`
